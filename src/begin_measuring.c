01947529a0a140cfed9850b6e8e38b6b55e576a8
Hesham Salman <hesham8@gmail.com>
2015-10-01 16:13:59 -0400

    Implemented screen capturing

--- src/begin_measuring.c
+++ src/begin_measuring.c
@@ -1,6 +1,7 @@
 /*******************************************************************************
   * Invokes tcpdump and getevent at the same time.
   * Prints out the pid of each process.
+  * TODO: RE-WRITE WITH PTHREADS INSTEAD OF FORK(). GETS MESSY WITH FORKS.
   */
 
 #include <stdio.h>
@@ -15,6 +16,7 @@
 
 #define BILLION 1000000000L
 
+// TODO: Refactor lines 103-118 and 75-89 and 43-52, anywhere where this grouping appears
 int main(int argc, char const *argv[]) {
   struct timespec mono, real_time;
   const uint64_t TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
@@ -27,12 +29,9 @@ int main(int argc, char const *argv[]) {
     printf("timespec2str failed!\n");
     return 1;
   }
-  pid_t tcpdump_thread, getevent_thread, meminfo_thread, cpuinfo_thread; //tcpdump_thread = tcpdump, getevent_thread = getevent
-
+  pid_t tcpdump_thread, getevent_thread, meminfo_thread, cpuinfo_thread, screenshot_thread;
   tcpdump_thread = fork();
-  getevent_thread = fork();
-  meminfo_thread = fork();
-  cpuinfo_thread = fork();
+
 
   if (tcpdump_thread == 0) {
     /* TCPDump thread */
@@ -54,44 +53,105 @@ int main(int argc, char const *argv[]) {
     strcat(filename, ".");
     strcat(filename, nsecbuf);
     strcat(filename, ".pcap");
+    int fd = open(filename, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+    dup2(fd, 1);
+    dup2(fd, 0);
+    close(fd);
 
     execlp("/data/local/tcpdump", "/data/local/tcpdump", "-i",  "any", "-p",
            "-s", "0", "-w", filename, NULL);
   }
-  if (getevent_thread == 0) {
-    int fd = open("recordedEvents", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
-    dup2(fd, 1);
-    close(fd);
-    execlp("getevent", "getevent", "-tt", NULL);
-  }
+  else {
+    getevent_thread = fork();
+    if (getevent_thread == 0) {
+      int fd = open("recordedEvents", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+      dup2(fd, 1);
+      dup2(fd, 0);
+      close(fd);
+      execlp("getevent", "getevent", "-tt", NULL);
+    } else {
+      meminfo_thread = fork();
+      if (meminfo_thread == 0) {
+        int fd = open("meminfo.dat", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+        dup2(fd, 1);
+        dup2(fd, 0);
+        close(fd);
+        while (1) {
+          synchronize_time(mono, real_time);
+          long sec = mono.tv_sec;
+          long nsec = mono.tv_nsec;
+          const int n = snprintf(NULL, 0, "%lu", sec);
+          char secbuf[n+1];
+          snprintf(secbuf, n+1, "%lu", sec);
 
-  if (meminfo_thread == 0) {
-    int fd = open("meminfo.dat", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
-    dup2(fd, 1);
-    close(fd);
-    while (1) {
-      synchronize_time(mono, real_time);
-      // TODO: Print the time (MONOTONIC and REAL)
-      execlp("dumpsys", "meminfo", filename, NULL);
-      sleep(500);
+          const int n2 = snprintf(NULL, 0, "%lu", nsec);
+          char nsecbuf[n2 + 1];
+          snprintf(nsecbuf, n2 + 1, "%lu", nsec);
+          if (timespec2str(timestr, TIME_FMT, &real_time) != 0) {
+            printf("timespec2str failed!\n");
+            return 1;
+          }
+          printf("%s.%s\n", secbuf, nsecbuf);
+          printf("%s\n", timestr);
+
+          execlp("dumpsys", "dumpsys", "meminfo", NULL);
+          sleep(500);
+        }
+      } else {
+        cpuinfo_thread = fork();
+        if (cpuinfo_thread == 0) {
+          int fd = open("cpuinfo.dat", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
+          dup2(fd, 1);
+          close(fd);
+          while (1) {
+            synchronize_time(mono, real_time);
+            long sec = mono.tv_sec;
+            long nsec = mono.tv_nsec;
+            const int n = snprintf(NULL, 0, "%lu", sec);
+            char secbuf[n+1];
+            snprintf(secbuf, n+1, "%lu", sec);
+
+            const int n2 = snprintf(NULL, 0, "%lu", nsec);
+            char nsecbuf[n2 + 1];
+            snprintf(nsecbuf, n2 + 1, "%lu", nsec);
+            if (timespec2str(timestr, TIME_FMT, &real_time) != 0) {
+              printf("timespec2str failed!\n");
+              return 1;
+            }
+            printf("%s.%s\n", secbuf, nsecbuf);
+            printf("%s\n", timestr);
+            execlp("dumpsys", "dumpsys", "cpuinfo",  NULL);
+            sleep(500);
+          }
+        } else {
+          screenshot_thread = fork();
+
+          if (screenshot_thread == 0) {
+            int i = 0;
+            while (1) {
+              char filename[255];
+              snprintf(filename, sizeof(filename), "/data/local/screenshots/screen%d.png", i);
+              execlp("screencap", "screencap", "-p", filename, NULL);
+              sleep(500);
+              i++;
+            }
+          } else {
+            int user_signal = -1;
+            printf("Type 0 to end measuring.\n");
+            scanf("%d", &user_signal);
+
+            if (!user_signal) {
+              terminate_child(meminfo_thread);
+              terminate_child(cpuinfo_thread);
+              terminate_child(tcpdump_thread);
+              terminate_child(getevent_thread);
+              terminate_child(screenshot_thread);
+            }
+          }
+        }
+      }
     }
   }
-
-  if (cpuinfo_thread == 0) {
-    int fd = open("meminfo.dat", O_RDWR|O_CREAT, S_IRUSR|S_IWUSR);
-    dup2(fd, 1);
-    close(fd);
-    while (1) {
-      synchronize_time(mono, real_time);
-      // TODO: Print the time (MONOTONIC and REAL)
-      execlp("dumpsys", "cpuinfo", filename, NULL);
-      sleep(500);
-    }
-  }
-
-  //TODO: Have thread for screenshots
-
-  //TODO: Have termination wait on the parent process. 
   return 0;
 }
 
